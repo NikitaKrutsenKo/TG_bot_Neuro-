a65c2649c53b354d6c3130ae21f59ffbd7c6d839
NikitaKrutsenKo <niankr5@gmail.com>
2024-07-02 14:59:55 +0300

    keyboards_names_update

--- bot_logic/handlers.py
+++ bot_logic/handlers.py
@@ -1,18 +1,32 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
-from .keyboards import topics, custom_keyboard
+from .keyboards import (
+main_features_keyboard, 
+main_features_list,
+neuro_type_list
+)
 
 router = Router()
 
 @router.message(Command('start'))
 async def send_welcome(message: Message):
-    await message.reply("Привіт, це тестова версія кнопок, виберіть тему:", reply_markup = custom_keyboard)
+    await message.reply("Привіт, це тестова версія кнопок, виберіть задачу:", reply_markup = main_features_keyboard)
 
-@router.message(lambda message: message.text in topics)
+
+@router.message(lambda message: message.text in main_features_list)
+async def send_feature_menu(message: Message):
+    feature_type = main_features_list.get(message.text, None)
+    if feature_type != None:
+        await message.answer('Ви перейшли у меню задачі '+ message.text + '. Оберіть наступну дію', reply_markup = feature_type)
+    else:
+        await message.answer('Або такої задачі нема, або сталася помилка. Спробуйте ще раз')
+
+
+@router.message(lambda message: message.text in neuro_type_list)
 async def send_topic_message(message: Message):
-    topic_message = topics.get(message.text, "Тема не знайдена")
-    await message.reply(topic_message, parse_mode='Markdown')
+    neuro_type_message = neuro_type_list.get(message.text, "Тема не знайдена")
+    await message.reply(neuro_type_message, parse_mode='Markdown')
 
 
 
--- bot_logic/keyboards.py
+++ bot_logic/keyboards.py
@@ -1,8 +1,21 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+main_features_keyboard = ReplyKeyboardMarkup(keyboard=
+        [[KeyboardButton(text='Навігатор')]],        
+    resize_keyboard=True, 
+    input_field_placeholder='Оберіть задачу'
+)
 
-custom_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Video')]])
+neuro_type_keyboard = ReplyKeyboardMarkup(keyboard=
+        [[KeyboardButton(text='Відео')]],        
+    resize_keyboard=True, 
+    input_field_placeholder='Оберіть тип нейронки'
+)
 
-topics = {
-    'Video': 'Відповідь є [тут](https://t.me/c/2156359075/2/3)',
+main_features_list = {
+    'Навігатор': neuro_type_keyboard
+}
+
+neuro_type_list = {
+    'Відео': 'Відповідь є [тут](https://t.me/c/2156359075/2/3)',
 }
